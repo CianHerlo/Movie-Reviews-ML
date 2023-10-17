259dd00ae01e071fcb3f066f3c14c9bff8464b41
Cian Herlihy <cian.herlihy@mycit.ie>
2023-10-18 00:17:41 +0100

    Commenting and removal of unused function from previous version

--- Cian Herlihy - R00205604 - Assignment1.py
+++ Cian Herlihy - R00205604 - Assignment1.py
@@ -1,8 +1,3 @@
-#
-#   Cian Herlihy - R00205604
-#
-#   Machine Learning Assignment 1
-#
 import re
 import pandas as pd
 import math
@@ -11,73 +6,72 @@ from sklearn import metrics
 from sklearn import neighbors
 import matplotlib.pyplot as plt
 
+#
+#   Cian Herlihy - R00205604
+#
+#   Machine Learning Assignment 1
+#
+
 FILE_NAME = 'movie_reviews.xlsx'
 
 
-def load_excel_file(file):  # Task 1 part 1
+def load_excel_file(file):
     print("Task 1: Loading Excel File")
-    df = pd.read_excel(file)  # Reads Excel file and sets it to df (dataframe)
-    return df  # Return Dataframe
+    df = pd.read_excel(file)    # Reads Excel file and sets it to df (dataframe)
+    return df                   # Return Dataframe
 
 
-def separate_data(df):  # Task 1 part 2
+def separate_data(df):
     print("Task 1: Split data")
-    training_df = df[df['Split'] == 'train']  # Training Dataframe
-    test_df = df[df['Split'] == 'test']  # Test Dataframe
+    training_df = df[df['Split'] == 'train']                # Training Dataframe
+    test_df = df[df['Split'] == 'test']                     # Test Dataframe
 
-    training_data = training_df['Review'].tolist()
-    training_labels = training_df['Sentiment'].tolist()
-    test_data = test_df['Review'].tolist()
-    test_labels = test_df['Sentiment'].tolist()
+    training_data = training_df['Review'].tolist()          # Gather Reviews into List for Training
+    training_labels = training_df['Sentiment'].tolist()     # Gather Sentiments into List for Training
+    test_data = test_df['Review'].tolist()                  # Gather Reviews into List for Test Data
+    test_labels = test_df['Sentiment'].tolist()             # Gather Sentiments into List for Test Data
 
-    num_pos_training = training_labels.count('positive')
-    num_neg_training = training_labels.count('negative')
-    num_pos_test = test_labels.count('positive')
-    num_neg_test = test_labels.count('negative')
+    num_pos_training = training_labels.count('positive')    # Get Sum of Positive Reviews in Training Data
+    num_neg_training = training_labels.count('negative')    # Get Sum of Negative Reviews in Training Data
+    num_pos_test = test_labels.count('positive')            # Get Sum of Positive Reviews in Test Data
+    num_neg_test = test_labels.count('negative')            # Get Sum of Negative Reviews in Test Data
 
-    print(f"Training Data - Positive | Negative: {num_pos_training} | {num_neg_training}")
-    print(f"Test Data     - Positive | Negative: {num_pos_test} | {num_neg_test}")
+    print(f"Training Data - Positive | Negative: {num_pos_training} | {num_neg_training}")  # Print Counts for Training Data
+    print(f"Test Data     - Positive | Negative: {num_pos_test} | {num_neg_test}")          # Print Counts for Test Data
 
-    return training_data, training_labels, test_data, test_labels
+    return training_data, training_labels, test_data, test_labels   # Return Datasets
 
 
 def count_positive_negative_reviews(df):
     print("Task 4: Count positive / negative reviews")
-    sum_pos = df['Sentiment'].eq('positive').sum()
-    sum_neg = df['Sentiment'].eq('negative').sum()
-    return sum_pos, sum_neg
+    sum_pos = df['Sentiment'].eq('positive').sum()  # Get Sum of Total Positive Reviews
+    sum_neg = df['Sentiment'].eq('negative').sum()  # Get Sum of Total Negative Reviews
+    return sum_pos, sum_neg                         # Return Sums
 
 
 def filter_reviews(reviews, min_word_length, min_word_appearances):
     print("Task 2: Filter Words from Reviews")
-    # Create a dictionary to keep track of word counts
-    word_counts = {}
+    word_counts = {}        # Create Dictionary for Keeping Word Popularity
+    filtered_words = []     # Create Empty List for Words Being Filtered
+    for review in reviews:  # Loop through all Reviews
 
-    # Process each review in the list
-    for review in reviews:
-        # Make the review lowercase and remove non-alphanumeric characters
         # https://stackoverflow.com/questions/6323296/python-remove-anything-that-is-not-a-letter-or-number
+        # Use Regex to get only Alpha-Numeric Characters and Make Reviews Lowercase
+        # I had another way of doing this previously which was much slower but found this on StackOverflow
         cleaned_review = re.sub(r'[^a-zA-Z0-9 ]', '', review.lower())
 
-        # Split the cleaned review into words
-        words = cleaned_review.split()
+        words = cleaned_review.split()          # Split Review into Individual Words
+        for word in words:                      # Loop Through each Word in Reviews
+            if len(word) >= min_word_length:    # Check if Word meets Minimum Length Requirements
+                if word in word_counts:         # If Word is already added to Dictionary
+                    word_counts[word] += 1      # Increment by 1
+                else:                           # Word Does Not Appear in Dictionary
+                    word_counts[word] = 1       # Add Word to Dictionary and Set Value to 1
 
-        # Iterate through the words in the review
-        for word in words:
-            # Check if the word meets the minimum length requirement
-            if len(word) >= min_word_length:
-                # Update word counts in the dictionary
-                if word in word_counts:
-                    word_counts[word] += 1
-                else:
-                    word_counts[word] = 1
-
-    # Create a list of filtered words that meet the minimum appearance requirement
-    filtered_words = []
-    for word, count in word_counts.items():
-        if count >= min_word_appearances:
-            filtered_words.append(word)
-    return filtered_words
+    for word, count in word_counts.items():     # Word & Count is iterated through in a loop of all word_counts key-value pairs
+        if count >= min_word_appearances:       # If Word Appearances is >= Minimum Word Count Param
+            filtered_words.append(word)         # Add Word to filtered_words List
+    return filtered_words                       # Return List of Filtered Words
 
 
 def featured_word_count_in_reviews(review_data, review_labels, filtered_words):
@@ -108,33 +102,7 @@ def featured_word_count_in_reviews(review_data, review_labels, filtered_words):
     return word_counts_positive, word_counts_negative, word_occurrence_count
 
 
-def calculate_priors_and_likelihoods(words_dict, training_data, training_labels):  # Task 4
-    smooth_factor = 1
-    likelihoods = {}
-    pos_reviews = []
-    neg_reviews = []
-    word_list = list(words_dict.keys())
-    for i, review in enumerate(training_data):
-        if training_labels[i] == "positive":
-            pos_reviews.append(review)
-        elif training_labels[i] == "negative":
-            neg_reviews.append(review)
-
-    total_reviews = len(pos_reviews) + len(neg_reviews)
-    prior_pos = len(pos_reviews) / total_reviews
-    prior_neg = len(neg_reviews) / total_reviews
-
-    for word in word_list:
-        count_pos = pos_reviews.count(word) + smooth_factor
-        count_neg = neg_reviews.count(word) + smooth_factor
-        likely_pos = count_pos / (len(pos_reviews) + smooth_factor * len(word_list))
-        likely_neg = count_neg / (len(neg_reviews) + smooth_factor * len(word_list))
-        likelihoods[word] = (likely_pos, likely_neg)
-
-    return prior_pos, prior_neg, likelihoods
-
-
-def calculate_likelihoods(word_list, pos_counts, neg_counts):  # Task 4
+def calculate_likelihoods(word_list, pos_counts, neg_counts):
     print("Task 4: Calculate Likelihoods")
     likelihoods = {}
     for word in word_list:
@@ -150,7 +118,7 @@ def calculate_likelihoods(word_list, pos_counts, neg_counts):  # Task 4
     return likelihoods
 
 
-def calculate_priors(total_pos_reviews, total_neg_reviews):  # Task 4 part 2
+def calculate_priors(total_pos_reviews, total_neg_reviews):
     print("Task 4: Calculate Priors")
     prior_pos = total_pos_reviews / (total_pos_reviews + total_neg_reviews)
     prior_neg = total_neg_reviews / (total_pos_reviews + total_neg_reviews)
@@ -183,6 +151,36 @@ def predict_sentiment(new_review, prior_pos, prior_neg, likelihoods):
     return prediction
 
 
+def k_fold_cross_validation(classifier, data, labels, k):
+    accuracies = []
+    kf = model_selection.KFold(n_splits=k, shuffle=True, random_state=42)
+
+    for train_index, eval_index in kf.split(data):
+        train_data, eval_data = [data[i] for i in train_index], [data[i] for i in eval_index]
+        train_labels, eval_labels = [labels[i] for i in train_index], [labels[i] for i in eval_index]
+
+        # Train the classifier on the training data
+        classifier.fit(train_data, train_labels)
+
+        # Make predictions on the evaluation data
+        predictions = classifier.predict(eval_data)
+
+        # Calculate accuracy
+        accuracy = calculate_accuracy(eval_labels, predictions)
+        accuracies.append(accuracy)
+
+    mean_accuracy = sum(accuracies) / len(accuracies)
+    return mean_accuracy
+
+
+def calculate_accuracy(true_labels, predicted_labels):
+    # Calculate the accuracy of predictions
+    correct = sum(1 for true, pred in zip(true_labels, predicted_labels) if true == pred)
+    total = len(true_labels)
+    accuracy = correct / total
+    return accuracy
+
+
 def main():  # Main Function
     # Task 1
     main_df = load_excel_file(FILE_NAME)
@@ -200,6 +198,7 @@ def main():  # Main Function
     prediction = predict_sentiment("This movie was fantastic, exhilarating, and tremendously choreographed. " +
                                    "Close to perfection", prior_pos, prior_neg, likelihoods)
     # Task 6
+    mean_accuracy = k_fold_cross_validation(classifier, training_data, training_labels, 10)
 
 
 if __name__ == '__main__':
